aac5bd624c2352b8b1bfa90738d8b8fd63bfb776
Matheus Gabriel Werny de Lima <matheusgwdl@protonmail.com>
2022-01-29 13:04:14 +0100

    Update.

--- client.py
+++ client.py
@@ -5,43 +5,31 @@ import time
 s: socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # IP of the receiver
-ip = input("IP-Adresse: ")
+ip: str = input("IP-Adresse: ")
 
-#### set send rate per second
-#sendRateBytesPerSecond = 1024 * 1000
+# The counter for the packets.
+counter: int = 0
 
-### set runtime in s
-#run_time = 2
-#time.time() returns actual time in seconds
-#t_end = time.time() + run_time 
-
-### calculate total nr of packets
-#total_packets = (sendRateBytesPerSecond/1024) * run_time
-
-#### use a counter for sequencing
-counter = 0
-
-#while time.time() < t_end:
-#### send 120 MB , then stop for 0.48 secs - repeat 30 times
-for o in range(30):
-    for i in range(120000):
-
-        now = time.time()
-
-        ### create data of 1024 bytes with the counter as Input
-        data = counter.to_bytes(1024, byteorder="big")
-        #send the data to receiver
+# Send 120 MB then stop for 0.48 seconds. Repeat 30 times.
+for j in range(30):
+    for k in range(120000):
+        now: float = time.time()
+        data: bytes = counter.to_bytes(1000, byteorder="big")
         s.sendto(data, (ip, 50000))
         counter += 1
 
     time.sleep(0.48)
 
-#### Ausbreitungsverzögerung = 35786 km /300000 km/s = 0.12 s
-#### 0.24 s bis zum anderen Empfänger auf der Erden
-#### Datenrate = 500 Mb/s
-#### Delay = 0.24 s
-#### datarate / delay produkt =  120 Mb 
+# === Rechnung ===
+# Ausbreitungsverzögerung = 35786 km / 300000 km/s = 0,12 s
+# 0,24 s bis zum anderen Empfänger auf der Erde, da von Klient zu Satellit, von Satellit zum Empfänger.
+# Datenrate = 500 MB/s
+# Delay = 0.24 s
+# Datarate-Delay-Produkt = 120 MB
 
-# Ich kann 120 MB auf den Kanal legen, um den Kanal effizient zu nutzen, dann kommen die ersten Pakete an (Überprüfung, ob Pakete vollständig sind, NACK's schicken..)
-# nach 0.48 s sind erst alle Daten vollständig eingetroffen
-# danach sendet nach Möglichkeit der Empfänger einen Bloom Filter zurück mit nicht empfangegen Frames, um den Kanal nicht zu belasten
+# === Zusammenfassung ===
+# Ich kann 120 MB auf den Kanal legen, um den Kanal effizient zu nutzen.
+# Dann kommen die ersten Pakete an. Zur Überprüfung, ob alle Pakete vollständig sind, NACKs schicken.
+# Nach 0,48 s sind erst alle Daten vollständig eingetroffen.
+# Danach sendet nach Möglichkeit der Empfänger einen Bloom-Filter zurück mit nicht empfangegen Frames,
+# um den Kanal nicht zu belasten.
